3dda1ff000213ec57b3235016eabc7953a29cd92
CBT RPi <cbt@cbt.com>
2024-03-10 13:23:54 -0400

    moved picoros

--- ros2_ws/README.md
+++ ros2_ws/README.md
@@ -2,3 +2,6 @@
 ros2 launch sllidar_ros2 sllidar_a1_launch.py
 # MicroROS
 ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/serial/by-id/usb-Arduino_RaspberryPi_Pico_304C61E62E421B93-if00
+# Send test command
+ros2 topic pub --once /pico_motor_commands mren_interfaces/msg/PicoMotorCommands "{left_wheel_velocity: 0.0, right_wheel_velocity: 0.0}"
+ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 1, y: 0, z: 0},angular: {x: 0, y: 0, z: 0}}"
--- ros2_ws/src/robot_driver/src/robot_driver.cpp
+++ ros2_ws/src/robot_driver/src/robot_driver.cpp
@@ -8,6 +8,10 @@
 #include "mren_interfaces/msg/pico_sensor_output.hpp"
 #include "mren_interfaces/msg/pico_motor_commands.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -16,22 +20,48 @@ class RobotDriver : public rclcpp::Node {
 
 	public:
 		RobotDriver() : Node("robot_driver") {
-			publisher_ = this->create_publisher<std_msgs::msg::String>("testpub", 10);
+			odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+
 			pmc_publisher_ = this->create_publisher<mren_interfaces::msg::PicoMotorCommands>("pico_motor_commands", 10);
+
 			pso_subscription_ = this->create_subscription<mren_interfaces::msg::PicoSensorOutput>("pico_sensor_output", 10, std::bind(&RobotDriver::pso_callback, this, _1));
+
 			cmd_vel_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&RobotDriver::cmd_vel_callback, this, _1));
+
+			tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
 			width = 0.3; // ROBOT WIDTH
+			dt = 0.1;
 		}
 	private:
 		double width;
-		rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+		double dt;
+		double x;
+		double y;
+		double th;
+		double vx;
+		double vy;
+		double vth;
+		rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
 		rclcpp::Subscription<mren_interfaces::msg::PicoSensorOutput>::SharedPtr pso_subscription_;
 		rclcpp::Publisher<mren_interfaces::msg::PicoMotorCommands>::SharedPtr pmc_publisher_;
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
+		std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 		void pso_callback(const mren_interfaces::msg::PicoSensorOutput::SharedPtr msg) const {
-			auto message = std_msgs::msg::String();
-			message.data = "Hello, World! " + std::to_string(msg->left_wheel_velocity);
-			publisher_->publish(message);
+			geometry_msgs::msg::TransformStamped t;
+			t.header.stamp = now();
+			t.header.frame_id = "odom";
+			t.child_frame_id = "base_link";
+			tf2::Quaternion q;
+			q.setRPY(0,0,0); // TODO Set Y to angle
+			(void)msg;
+			auto odom = nav_msgs::msg::Odometry();
+			odom.header.stamp = now();
+			odom.header.frame_id = "odom";
+			odom.child_frame_id = "base_link";
+			//TODO
+			tf_broadcaster_->sendTransform(t);
+			odom_publisher_->publish(odom);
 		}
 		void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const {
 			auto message = mren_interfaces::msg::PicoMotorCommands();
